86346bd72d40d925818cc2d25860f70506253d44
Haiyang <hjin317@aucklanduni.ac.nz>
2021-07-17 20:59:08 +0400

    add more utilities

--- utilities/fs.py
+++ utilities/fs.py
@@ -2,6 +2,7 @@ import os
 import re
 from itertools import compress
 
+# subject code and session lists
 def subjdir(stru_path=os.getenv('SUBJECTS_DIR'), str_pattern='', setdir=True):
     """This function set up 'SUBJECTS_DIR' and output the subject code list.
 
@@ -15,12 +16,12 @@ def subjdir(stru_path=os.getenv('SUBJECTS_DIR'), str_pattern='', setdir=True):
 
     Returns:
         stru_path (str): path to the structural folder.
-        subj_list (str): a list of subject codes.
-        
-    Created by Haiyang Jin (17-7-2021)
+        subj_list (str list): a list of subject codes.
     """
     if bool(stru_path):
-        setdir=0
+        setdir=False
+    else:
+        stru_path=os.getenv('SUBJECTS_DIR')
     
     # my sceret default path to 'fsaverage'
     if stru_path == 'myfs':
@@ -49,10 +50,11 @@ def sesslist(sessid='sessid*', func_path = os.getenv('FUNCTIONALS_DIR')):
         Exception: sessid should only match one session id file.
 
     Returns:
-        str: a list of session names.
-        
-    Created by Haiyang Jin (17-7-2021)
+        str list: a list of session names.
     """
+    if not bool(func_path):
+        func_path = os.getenv('FUNCTIONALS_DIR')
+    
     if os.sep not in sessid:
         sessid_list = [f for f in os.listdir(func_path) if re.match(sessid, f) and '.' not in f]
         n_sessid = len(sessid_list)
@@ -76,14 +78,14 @@ def sesslist(sessid='sessid*', func_path = os.getenv('FUNCTIONALS_DIR')):
     return sess_list
     
     
-    
+# convert/extract information
 def tocontrast(fn_list, separator='.', con_sign='-vs-'):
     """This function obtains the contrast name from the strings (e.g., a label 
     name when the label name is something like roi.lh.f13.f-vs-o.*label). It 
     will obtain the strings around '-vs-'.
 
     Args:
-        fn_list (str): a list of strings (e.g., label names).
+        fn_list (str list): a list of strings (e.g., label names).
         separator (str, optional): delimiter used to parse the fnList into 
             multiple parts and contrast name will be one of the strings. 
             Defaults to '.', which is for obtaining contrast from label name. 
@@ -92,9 +94,7 @@ def tocontrast(fn_list, separator='.', con_sign='-vs-'):
             the contrast names. Defaults to '-vs-'.
 
     Returns:
-        str: a list of contrast names.
-        
-    Created by Haiyang Jin (17-7-2021)
+        str list: a list of contrast names.
     """
     # split the fn_list by separator
     fn_sep = [s.split(separator) for s in fn_list]
@@ -115,10 +115,7 @@ def tohemi(filename, fn_only=True):
 
     Returns:
         str: the hemi information.
-        
-    Created by Haiyang Jin (17-7-2021)
     """
-    
     hemi_bool = [False, False]
     
     if os.sep in filename and fn_only:
@@ -134,9 +131,149 @@ def tohemi(filename, fn_only=True):
         hemi = list(compress(['lh', 'rh'], hemi_bool))[0]
     else:
         hemi = ''
-        raise Exception('Cannot determine if this file is for left or right hemisphere.')
+        print('Cannot determine if this file is for left or right hemisphere.')
         
     return hemi
 
+
+
+def to_hemi_multi(fn_list, fn_only=True):
+    """This function determine the hemispheres based on the filenames (if 'lh'
+    or 'rh' is included in the filename).
+
+    Args:
+        fn_list (str list): a list of filenames.
+        fn_only (bool, optional):  whether only check the filename; False:
+            also check the path. Defaults to True.
+
+    Returns:
+        str: the hemi information.
+    """
+    hemis = [tohemi(x, fn_only) for x in fn_list]
     
+    if len(set(hemis)) > 1:
+        print('\nThese files are for both hemispheres.')
     
+    return hemis
+
+
+    
+def tosig(fn_list):
+    """This function identify the significance p-value from the filename.
+
+    Args:
+        fn_list (str): a list of filenames.
+
+    Returns:
+        float: the p-value in FreeSurfer.
+    """
+    if isinstance(fn_list, str): # convert to list if it is not
+        fn_list = [fn_list]
+            
+    # find strings match f\d+ pattern
+    pattern = re.compile('f\d+')  
+    matches = [[p for p in pattern.finditer(f)] for f in fn_list] 
+    sig_str = [''.join([s.group() for s in m]) for m in matches]
+    
+    # remove f from the matched strings and convert to float
+    sig = [float(s[1:]) for s in sig_str]
+    
+    return sig
+        
+        
+
+def totemplate(ana_list, patterns=['fsaverage', 'self'], default_str='unknown'):
+    """This functions tries to identify the template used in the filename. By 
+    default, it will identify {'fsaverage', 'self'}. This function just to
+    test if the strings (e.g., 'fsaverage') are contained in the filenames.
+
+    Args:
+        ana_list (str list): a list of strings to be checked.
+        patterns (list, optional): the pattern strings. Defaults to ['fsaverage', 'self'].
+        default_str (str, optional): the default out strings if no patterns are
+            identified in filenames. Defaults to 'unknown'.
+
+    Returns:
+        [str list]: the templates for each filename. 'unkonwn' denotes
+            none of the patterns were found in the filename. 'multiple'  
+            denotes at least two of the patterns were found in the filename.
+    """
+    if isinstance(ana_list, str):
+        ana_list = [ana_list]
+
+    n_pattern = len(patterns)       
+            
+    if isinstance(default_str, str):
+        default_str = [default_str]
+    
+    # template for each combination between ana and pattern
+    tmp_template = [y if y in x else '' for x in ana_list for y in patterns]
+    
+    # join the patterns for each in ana_list
+    templates = [''.join(tmp_template[k:(k+n_pattern)]) for k in range(len(tmp_template)) if k%n_pattern==0]
+    
+    return templates
+
+
+
+def ana2con(ana_list, func_path=os.getenv('FUNCTIONALS_DIR')):
+    """This function reads the contrast names within the analysis folders.
+
+    Args:
+        ana_list (str list): list of analysis names.
+        func_path (str, optional): path to functional folder. Defaults 
+            to os.getenv('FUNCTIONALS_DIR').
+
+    Returns:
+        str list: list of contrast names.
+    """
+    if not bool(func_path):
+        func_path = os.getenv('FUNCTIONALS_DIR')
+    
+    if isinstance(ana_list, str):
+        ana_list = [ana_list]
+    
+    con_list = [None] * len(ana_list)
+    for i, a in enumerate(ana_list):
+        con_list[i] = [f.replace('.mat', '') for f in os.listdir(os.path.join(func_path, a)) if f.endswith('.mat')]
+
+    return con_list
+
+
+
+# visualization
+def colors(color):
+    """This function generates default rgb colors.
+
+    Args:
+        color (int list): which colors (rows) in 'colors' to be output.
+
+    Returns:
+        list: nested list of colors
+    """
+    clr = [
+        [1, 1, 1], # white
+        [1, 1, 0], # yellow
+        [1, 0, 1], # magneta
+        [0, 1, 0], # green
+        [.5, 0, 1], # purple
+        [0, 1, 1], # blue
+        [0, 0, 0], # black
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]
+    ]
+    
+    n_clr = len(clr)
+    
+    if not bool(color):
+        n_color = range(n_clr)
+        
+    out_colors = [clr[c] for c in range(n_clr) if c in color]
+        
+    return out_colors
+        
+
+
