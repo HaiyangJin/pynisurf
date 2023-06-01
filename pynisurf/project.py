341e57ce62a86d2d0eb1f837245d56e89f9f4052
Haiyang Jin <hjin317@aucklanduni.ac.nz>
2023-06-01 08:23:42 +0400

    fix bug for not setting bidsDir

--- pynisurf/project.py
+++ pynisurf/project.py
@@ -9,27 +9,32 @@ class project:
     Create a project and store the relevant information.
     '''
    
-    def __init__(self, fsdir='', bidsdir='', subjdir='', funcdir='', str_pattern='sub-*'):
+    def __init__(self, fsdir, bidsdir='', subjdir='', funcdir='', str_pattern='sub-*'):
         
         fs.setup(fsdir) # setup FreeSurfer
         self.fsversion = fs.version('', False)
         
         # set up BIDS
-        self.bidsdir, self.bidslist = bids.bidsdir(bidsdir, str_pattern)
-        self.sourcedata = os.path.join(self.bidsdir, 'sourcedata')
+        if bool(bidsdir):
+            self.bidsdir, self.bidslist = bids.bidsdir(bidsdir, str_pattern)
+            self.sourcedata = os.path.join(self.bidsdir, 'sourcedata')
         
         # set up SUBJECTS_DIR & FUNCTIONALS_DIR
-        if not bool(subjdir):
+        if bool(subjdir):
             subjdir = os.path.join(self.bidsdir, 'derivatives', 'subjects')
         if os.path.isdir(subjdir):
             self.updatesubjdir(subjdir, str_pattern)
         # self.subjdir, self.subjlist = fs.subjdir(subjdir, str_pattern)
             
-        if not bool(funcdir):
+        if bool(funcdir):
             funcdir = os.path.join(self.bidsdir, 'derivatives', 'functionals')
         if os.path.isdir(funcdir):
             self.updatefuncdir(funcdir, str_pattern)
         # self.funcdir, self.funclist = fs.funcdir(funcdir, str_pattern)
+        
+    def updatebidsdir(self, bidsdir, str_pattern='sub-*'):
+        self.bidsdir, self.bidslist = bids.bidsdir(bidsdir, str_pattern)
+        self.sourcedata = os.path.join(self.bidsdir, 'sourcedata')
     
     def updatesubjdir(self, subjdir, str_pattern='sub-*'):
         self.subjdir, self.subjlist = fs.subjdir(subjdir, str_pattern)
